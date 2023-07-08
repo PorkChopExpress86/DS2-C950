e6f3aa4366f4d47c123988ddccb000577726014e
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2023-07-08 18:02:56 -0500

    Added more methods to help with main

--- helper.py
+++ helper.py
@@ -1,6 +1,9 @@
 from HashTable import HashTable
 from Package import Package
+from Truck import Truck
 import csv
+import numpy as np
+from Genetic import *
 
 
 def fill_hash_table(packages_csv: str) -> HashTable:
@@ -37,3 +40,153 @@ def create_distance_matrix(distance_table_path: str) -> list:
             row = [float(i) if i != "None" else None for i in row]
             distance_list.append(row)
     return distance_list
+
+
+def convert_to_hours(some_time: str) -> float:
+    h, m, s = some_time.split(":")
+    hrs = float(h)
+    hrs += float(m) / 60
+    hrs += float(s) / (60 * 60)
+    return hrs
+
+
+def create_address_dict(addresses_csv_path):
+    """
+    Create a dictionary of the address and their index. The address is the key and the index are the value.
+    This will be used in the package look up and for the distance table lookup, the higher value will go to
+    the row and the lower number will be the column.
+
+    :param addresses_csv_path: string path of the csv containing all the address
+    :return: dictionary with address as the key and int as the value.
+    Big(O): O(n) looping over each line in the csv
+    """
+    address_dict = {}
+    with open(addresses_csv_path) as csv_file:
+        for index, line in enumerate(csv_file):
+            address_dict[line.replace("\n", "").split(",")[1]] = index
+    return address_dict
+
+
+def create_address_list(addresses_csv_path):
+    address_list = []
+    with open(addresses_csv_path) as csv_file:
+        for index, line in enumerate(csv_file):
+            address_list.append(line.replace("\n", "").split(",")[1])
+    return address_list
+
+
+def convert_package_id_to_address_index(
+    truck_packages: list, addresses_index: dict, hash_map: HashTable
+) -> list:
+    """Convert the trucks package list of id's into the indexes of the address list"""
+    truck_address_list = []
+    for package_id in truck_packages:
+        package_address = hash_map.get_item(package_id).address
+        address_index = addresses_index[package_address]
+        truck_address_list.append(address_index)
+    return list(set(truck_address_list))
+
+
+def create_address_dict_int_to_address(addresses_csv_path):
+    """
+    Create a dictionary of the address and their index. The address is the key and the index are the value.
+    This will be used in the package look up and for the distance table lookup, the higher value will go to
+    the row and the lower number will be the column.
+
+    :param addresses_csv_path: string path of the csv containing all the address
+    :return: dictionary with address as the key and int as the value.
+    Big(O): O(n) looping over each line in the csv
+    """
+    address_dict = {}
+    with open(addresses_csv_path) as csv_file:
+        for index, line in enumerate(csv_file):
+            address_dict[index] = line.replace("\n", "").split(",")[1]
+    return address_dict
+
+
+def genetic_algorithm(
+    location_indexes,
+    adjacency_mat,
+    address_index,
+    hash_map,
+    truck,
+    n_population=50,
+    n_iter=1000,
+    selectivity=0.15,
+    p_cross=0.5,
+    p_mut=0.1,
+    print_interval=100,
+    return_history=False,
+    verbose=False,
+):
+    location_indexes = np.array(location_indexes)
+
+    pop = init_population(
+        location_indexes, adjacency_mat, address_index, n_population, hash_map, truck
+    )
+
+    best = pop.best
+    score = float("inf")
+    history = []
+
+    for i in range(n_iter):
+        pop.select(n_population * selectivity)
+        history.append(pop.score)
+        if verbose:
+            print(f"Generation {i}: {pop.score}")
+        # elif i % print_interval == 0:
+        #     print(f"Generation {i}: {pop.score}")
+        if pop.score < score:
+            best = pop.best
+            best = np.insert(best, 0, 0)
+            best = np.append(best, 0)
+            score = pop.score
+        children = pop.mutate(p_cross, p_mut)
+        pop = Population(children, pop.adjacency_mat, address_index, hash_map, truck)
+    if return_history:
+        return best, score
+    return best
+
+
+def convert_address_id_to_address(hash_map: HashTable, best: list, address_list: list):
+    delivery_route_address = []
+    delivery_route_package_id = []
+
+    # Get the addresses in the route
+    for address_id in best:
+        for address_index, address in enumerate(address_list):
+            if address_id == address_index:
+                delivery_route_address.append(address)
+    return delivery_route_address  # , delivery_route_package_id
+
+
+def truck_finish_time(truck: Truck, score: float):
+    departure_time = convert_to_hours(truck.departure_time)
+    total_time = departure_time + score / truck.speed
+    hours = int(total_time - (total_time % 1))
+    minutes = (total_time % 1) * 60
+    seconds = (minutes % 1) * 60
+    minutes = int(minutes)
+    seconds = int(round(seconds, 0))
+    if 0 <= hours < 10:
+        hours_str = "0" + str(int(hours))
+    else:
+        hours_str = str(hours)
+
+    if 0 <= minutes < 10:
+        minutes_str = "0" + str(minutes)
+    else:
+        minutes_str = str(minutes)
+
+    if 0 <= seconds < 10:
+        seconds_str = "0" + str(seconds)
+    else:
+        seconds_str = str(seconds)
+
+    return f"{hours_str}:{minutes_str}:{seconds_str}"
+
+
+def address_index_to_package_id(
+    hash_table: HashTable,
+):
+    pass
--- main.py
+++ main.py
@@ -1,1 +1,126 @@
-print("Running main")
+from helper import *
+from Genetic import *
+from Truck import Truck
+from Package import Package
+
+
+def load_truck_easy():
+    """Manual loading of the truck packages"""
+    truck1.packages = [7, 29, 19, 1, 13, 39, 20, 21, 4, 40, 14, 15, 16, 34]
+    truck2.packages = [18, 36, 3, 8, 30, 6, 31, 32, 5, 37, 38, 25, 26]
+    truck3.packages = [27, 35, 2, 33, 11, 28, 17, 12, 24, 23, 10, 22, 9]
+
+
+# Fill hash table
+hash_map = fill_hash_table("CSVFiles/packages.csv")
+
+# Create distance matrix
+distance_array = create_distance_matrix("CSVFiles/distance_table.csv")
+
+# Create address index
+address_index = create_address_dict("CSVFiles/addresses.csv")
+
+# Set up parameters and create truck objects
+truck_packages = {}
+speed = 18
+truck1 = Truck(
+    1, speed=speed, location="4001 S700 E", departure_time="08:00:00"
+)  # early departure, more time sensitive packages
+truck2 = Truck(
+    2, speed=speed, location="4001 S700 E", departure_time="09:05:00"
+)  # late arrival packages
+
+truck3 = Truck(3, speed=speed, location="4001 S700 E")  # EOD deliveries and left overs
+
+# Load packages in trucks
+load_truck_easy()
+
+print(truck1.packages)
+print(truck2.packages)
+print(truck3.packages)
+
+
+# Truck 1
+truck1_package_indexes = convert_package_id_to_address_index(
+    truck1.packages, address_index, hash_map
+)
+
+best1, score1 = genetic_algorithm(
+    truck1_package_indexes,
+    np.asarray(distance_array),
+    address_index,
+    hash_map,
+    truck1,
+    return_history=True,
+    verbose=False,
+)
+# Time to complete the route in hours
+truck1_total_time = score1 / truck1.speed
+
+truck1.finish_time = truck_finish_time(truck1, score1)
+
+print(
+    f"The best route for truck 1 is {best1}. \t\nThat will be {score1:.1f} miles. \t\nDeparture: {truck1.departure_time}, finish time: {truck1.finish_time}"
+)
+
+# Truck 2
+truck2_package_indexes = convert_package_id_to_address_index(
+    truck2.packages, address_index, hash_map
+)
+
+best2, score2 = genetic_algorithm(
+    truck2_package_indexes,
+    np.asarray(distance_array),
+    address_index,
+    hash_map,
+    truck2,
+    return_history=True,
+    verbose=False,
+)
+truck2_total_time = score2 / truck2.speed
+truck2.finish_time = truck_finish_time(truck2, score2)
+print(
+    f"The best route for truck 2 is {best2}. \t\nThat will be {score2:.1f} miles. \t\nDeparture: {truck2.departure_time}, finish time: {truck2.finish_time}"
+)
+
+# Truck 3
+if truck1.finish_time < truck2.finish_time:
+    t3_depart = truck1.finish_time
+else:
+    t3_depart = truck2.finish_time
+truck3.departure_time = t3_depart
+truck3_package_indexes = convert_package_id_to_address_index(
+    truck3.packages, address_index, hash_map
+)
+
+best3, score3 = genetic_algorithm(
+    truck3_package_indexes,
+    np.asarray(distance_array),
+    address_index,
+    hash_map,
+    truck3,
+    return_history=True,
+    verbose=False,
+)
+
+truck3_total_time = score3 / truck3.speed
+truck3.finish_time = truck_finish_time(truck3, score3)
+print(
+    f"The best route for truck 3 is {best3}. \t\nThat will be {score3:.1f} miles. \t\nDeparture: {truck3.departure_time}, finish time: {truck3.finish_time}"
+)
+total_distance = score1 + score2 + score2
+print(f"Total trip disance was {total_distance:.2f}")
+
+truck1_route_address = convert_address_id_to_address(hash_map, best1, address_index)
+truck2_route_address = convert_address_id_to_address(hash_map, best2, address_index)
+truck3_route_address = convert_address_id_to_address(hash_map, best3, address_index)
+
+print(
+    f"The best route for truck 1 is {truck1_route_address}. \t\nThat will be {score1} miles."
+)
+print(
+    f"The best route for truck 2 is {truck2_route_address}. \t\nThat will be {score2} miles."
+)
+print(
+    f"The best route for truck 3 is {truck3_route_address}. \t\nThat will be {score3} miles."
+)
