ee853264528e5b7ce10c8ef6dc39c264f0ba80ac
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2023-07-12 23:37:14 -0500

    Cleaned up comments and variables

--- Package.py
+++ Package.py
@@ -1,29 +1,28 @@
 class Package:
-    """
-    Package class that is initialized with the following parameters
-
-    :param id (int) of the package number
-    :param address (str) destination of the package
-    :param city (str) city of the destination
-    :param state (str) state of the destination
-    :param zipcode (int) zipcode of the destination, entered as an int and stored as a string
-    :param deadline (str) deadline of when the package must be delivered, in the format of "hh:mm:ss"
-    :param weight (int) weight of the package in pounds
-    :param note (str) text of the notes of the package
-    :param status (str) of the package, at the hub, en route or delivered
-    """
 
     def __init__(
-        self,
-        package_id: int,
-        address: str,
-        city: str,
-        state: str,
-        zipcode: str,
-        deadline: str,
-        weight: str,
-        note: str,
+            self,
+            package_id: int,
+            address: str,
+            city: str,
+            state: str,
+            zipcode: str,
+            deadline: str,
+            weight: str,
+            note: str,
     ) -> None:
+        """Package class that is initialized with the following parameters
+
+        :param id (int) of the package number
+        :param address (str) destination of the package
+        :param city (str) city of the destination
+        :param state (str) state of the destination
+        :param zipcode (int) zipcode of the destination, entered as an int and stored as a string
+        :param deadline (str) deadline of when the package must be delivered, in the format of "hh:mm:ss"
+        :param weight (int) weight of the package in pounds
+        :param note (str) text of the notes of the package
+        :param status (str) of the package, at the hub, en route or delivered
+        """
         self.id = package_id
         self.address = address
         self.city = city
